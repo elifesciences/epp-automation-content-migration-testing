515968ed79abbb7e767f9431becff16c137c0ac2
Nathan Lisgo <nathan@lisgo.co.uk>
2023-11-03 17:25:04 +0000

    make improvements to the script particularly as it was ignoring the first id

--- content-migration.js
+++ content-migration.js
@@ -1,5 +1,5 @@
-const syncFetch = require("sync-fetch");
-const fs = require('fs');
+const syncFetch = require('sync-fetch');
+const { readFileSync} = require('fs');
 
 const config = {
   id: 'enhanced_article',
@@ -33,27 +33,25 @@ const config = {
   debugWindow: false
 }
 
-
-
 const fetchAndParseManuscripts = () => {
   try {
-    return JSON.parse(fs.readFileSync('manuscripts.json', 'utf8'));
+    return JSON.parse(readFileSync('manuscripts.json', 'utf8'))['manuscripts'];
   } catch (err) {
     const manuscriptsJson = syncFetch('https://raw.githubusercontent.com/elifesciences/enhanced-preprints-client/master/manuscripts.json');
-    return manuscriptsJson.json();
+    return manuscriptsJson.json()['manuscripts'];
   }
 }
 
 const manuscripts = fetchAndParseManuscripts();
-const rppIds = Object.keys(manuscripts.manuscripts);
+const rppIds = Object.keys(manuscripts);
 
-const scenarios = rppIds.map((rppId) => ({
-  label: `Enhanced Article ${rppId}`,
-  url: `https://prod-automation--epp.elifesciences.org/reviewed-preprints/${rppId}`,
-  referenceUrl: `https://prod--epp.elifesciences.org/reviewed-preprints/${rppId}`,
-  removeSelectors: ["#CybotCookiebotDialog", "#assessment>.descriptors"]
-}))
-  .slice(1, 20);
+const scenarios = rppIds.slice(0, 20)
+  .map((rppId) => ({
+    label: `Enhanced Article ${rppId}`,
+    url: `https://prod-automation--epp.elifesciences.org/reviewed-preprints/${rppId}`,
+    referenceUrl: `https://prod--epp.elifesciences.org/reviewed-preprints/${rppId}`,
+    removeSelectors: ["#CybotCookiebotDialog", "#assessment>.descriptors"]
+  }));
 
 config.scenarios = scenarios;
 

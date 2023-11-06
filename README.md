2da5d15ffd37654aa5de0a3c3dc8dc21cb352934
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-11-06 10:40:57 +0000

    Add the ability to use a local manuscript.json (#3)
    * Add the abbility to use a local manuscript.json
    
    * make improvements to the script particularly as it was ignoring the first id
    
    * expand the documentation in the README.md
    
    * improve formatting
    
    * Add content-status script
    
    * Add instructions in README.md for content-status
    
    * feat: added docmap-status script and updated package.json
    
    - Removed two blank lines in content-status.js
    - Added a new file docmap-status.js with code for fetching and parsing manuscripts, checking URLs, and breaking down arrays into smaller chunks
    - Updated package.json to include a new script for running docmap-status.js
    
    ---------
    
    Co-authored-by: viktoria.mahmud <viktoria.nedyalkova@endava.com>

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 This is a one off script to compare content between two deployed versions of EPP after content ingestion has been automated.
 
 If you stumble upon this, it's probably only useful as a reference of what we did and how we did it.
+
+To create your reference images run:
+
+```shell
+yarn generate-reference
+```
+
+To compare the production instance of EPP with the reference images:
+
+```shell
+yarn run-test
+```
+
+To reduce the feedback loop and if you want to focus on specific manuscripts then you can create a manuscripts.json file in the root of this project. See this example:
+
+```json
+{
+  "manuscripts": {
+    "86961v2": {}
+  }
+}
+```
+
+To simply monitor whether the expected manuscripts are available without performing a visual comparison:
+
+```shell
+yarn content-status
+```
--- content-migration.js
+++ content-migration.js
@@ -1,4 +1,5 @@
-const syncFetch = require("sync-fetch");
+const syncFetch = require('sync-fetch');
+const { readFileSync } = require('fs');
 
 const config = {
   id: 'enhanced_article',
@@ -32,24 +33,25 @@ const config = {
   debugWindow: false
 }
 
-
-
 const fetchAndParseManuscripts = () => {
-  const manuscriptsJson = syncFetch('https://raw.githubusercontent.com/elifesciences/enhanced-preprints-client/master/manuscripts.json');
-  const manuscripts = manuscriptsJson.json();
-  return manuscripts;
+  try {
+    return JSON.parse(readFileSync('manuscripts.json', 'utf8'))['manuscripts'];
+  } catch (err) {
+    const manuscriptsJson = syncFetch('https://raw.githubusercontent.com/elifesciences/enhanced-preprints-client/master/manuscripts.json');
+    return manuscriptsJson.json()['manuscripts'];
+  }
 }
 
 const manuscripts = fetchAndParseManuscripts();
-const rppIds = Object.keys(manuscripts.manuscripts);
+const rppIds = Object.keys(manuscripts);
 
-const scenarios = rppIds.map((rppId) => ({
-  label: `Enhanced Article ${rppId}`,
-  url: `https://prod-automation--epp.elifesciences.org/reviewed-preprints/${rppId}`,
-  referenceUrl: `https://prod--epp.elifesciences.org/reviewed-preprints/${rppId}`,
-  removeSelectors: ["#CybotCookiebotDialog", "#assessment>.descriptors"]
-}))
-  .slice(1, 20);
+const scenarios = rppIds.slice(0, 20)
+  .map((rppId) => ({
+    label: `Enhanced Article ${rppId}`,
+    url: `https://prod-automation--epp.elifesciences.org/reviewed-preprints/${rppId}`,
+    referenceUrl: `https://prod--epp.elifesciences.org/reviewed-preprints/${rppId}`,
+    removeSelectors: ["#CybotCookiebotDialog", "#assessment>.descriptors"]
+  }));
 
 config.scenarios = scenarios;
 

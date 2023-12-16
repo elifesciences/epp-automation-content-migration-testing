8762663b10c03a1a128640d36a51cc5d6aea7ca3
Nathan Lisgo <nathan@lisgo.co.uk>
2023-12-16 00:29:36 +0000

    feat: Improve console output and update README
    The console output in `journal-api-status.js` has been updated for better readability.
    Also, added instructions in `README.md` on verifying the status of the automated instance of Journal API.

--- journal-api-status.js
+++ journal-api-status.js
@@ -4,6 +4,28 @@ const semiAutomatedUrl = 'https://prod--gateway.elifesciences.org/reviewed-prepr
 const automatedUrl = 'https://prod-automation--epp.elifesciences.org/api/reviewed-preprints';
 const batchSize = 50;
 
+const findSnippet = (id, snippets) => snippets.find((snippet) => snippet.id === id);
+
+const prepareSnippetForComparison = ({
+  id,
+  doi,
+  authorLine,
+  title,
+  subjects,
+  published,
+  statusDate,
+  pdf,
+}) => ({
+  id,
+  doi,
+  authorLine,
+  title,
+  subjects: subjects.sort((a, b) => a.id.localeCompare(b.id)),
+  published: new Date(published).toLocaleDateString('en-GB'),
+  statusDate: new Date(statusDate).toLocaleDateString('en-GB'),
+  ...(pdf ? { pdf } : {}),
+});
+
 const fetchSnippets = (url, page, perPage) => {
   const results = syncFetch(`${url}?order=asc&page=${page}&per-page=${perPage}`);
   return results.json();
@@ -17,18 +39,18 @@ const automatedSnippets = [];
 
 let page = 1;
 console.log(`Collecting semi-automated snippets (${semiAutomatedTotal})`);
-while (semiAutomatedSnippets.length < semiAutomatedTotal) {
+while (semiAutomatedSnippets.length < 50) {
   console.log(`gathering page ${page} of ${Math.ceil(semiAutomatedTotal / batchSize)}`);
-  semiAutomatedSnippets.push(...fetchSnippets(semiAutomatedUrl, page, batchSize)['items']);
+  semiAutomatedSnippets.push(...fetchSnippets(semiAutomatedUrl, page, batchSize)['items'].map((snippet) => prepareSnippetForComparison(snippet)));
   page++;
 }
 console.log(`Completed collection of semi-automated snippets!`);
 
 page = 1;
 console.log(`Collecting automated snippets (${automatedTotal})`);
-while (automatedSnippets.length < automatedTotal) {
+while (automatedSnippets.length < 50) {
   console.log(`gathering page ${page} of ${Math.ceil(automatedTotal / batchSize)}`);
-  automatedSnippets.push(...fetchSnippets(automatedUrl, page, batchSize)['items']);
+  automatedSnippets.push(...fetchSnippets(automatedUrl, page, batchSize)['items'].map((snippet) => prepareSnippetForComparison(snippet)));
   page++;
 }
 console.log(`Completed collection of automated snippets!`);
@@ -40,6 +62,32 @@ const msidsOnlyInSemiAutomated = semiAutomatedSnippetsMsids.filter((msid) => !au
 const msidsOnlyInAutomated = automatedSnippetsMsids.filter((msid) => !semiAutomatedSnippetsMsids.includes(msid));
 const msidsInBoth = semiAutomatedSnippetsMsids.filter((msid) => automatedSnippetsMsids.includes(msid));
 
-console.log(`msidsInBoth (${msidsInBoth.length})`);
+const different = [];
+const same = [];
+msidsInBoth.map((msid) => {
+  const s = findSnippet(msid, semiAutomatedSnippets);
+  const a = findSnippet(msid, automatedSnippets);
+
+  if (JSON.stringify(s) !== JSON.stringify(a)) {
+    different.push({
+      id: msid,
+      semiAutomated: s,
+      automated: a,
+    });
+
+    return false;
+  }
+
+  same.push({
+    id: msid,
+    semiAutomated: s,
+    automated: a,
+  });
+  return true;
+});
+
 console.log(`msidsOnlyInSemiAutomated (${msidsOnlyInSemiAutomated.length}) ${msidsOnlyInSemiAutomated.join(',')}`);
 console.log(`msidsOnlyInAutomated (${msidsOnlyInAutomated.length}) ${msidsOnlyInAutomated.join(',')}`);
+console.log(`msidsInBoth (${msidsInBoth.length})`);
+
+console.log(`different (${different.length} of ${msidsInBoth.length}): ${different.map(({ id }) => id).join(',')}`);
